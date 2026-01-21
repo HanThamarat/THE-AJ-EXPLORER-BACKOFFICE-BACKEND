c6884fa2635dbe266b81a1192008b6597e078d00
HanThamarat <HanThamarat@gmail.com>
2026-01-21 20:07:48 +0700

    hot-fix/fix-directory-path

--- src/adapters/http/prisma/clientBank.ts
+++ src/adapters/http/prisma/clientBank.ts
@@ -2,14 +2,21 @@ import { bankEntityType } from "../../../core/entity/clientBank";
 import { ClientBankRepositoryPort } from "../../../core/ports/clientBankRepositoryPort";
 import { prisma } from "../../database/data-source";
 import { Convertion } from "../../helpers/convertion";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const devMode = process.env.DEV || false;
 
 export class ClientBankDataSource implements ClientBankRepositoryPort {
     async findBankOption(): Promise<bankEntityType[]> {
         const result = await prisma.banking.findMany({});
 
         const mapResultFormat: bankEntityType[] = await Promise.all(result.map(async (item) => {
+            
+            const path = devMode ? item.bankPicture : `./dist/${item.bankPicture}`;
 
-            const base64 = await Convertion.FileToBase64WithPath(item.bankPicture as string);
+            const base64 = await Convertion.FileToBase64WithPath(path as string);
 
             return {
                 id: item.id,
