141ef22c5344fe3ed92a4411e721cedcf89ef394
HanThamarat <HanThamarat@gmail.com>
2026-01-30 14:21:38 +0700

    fix database

--- prisma/migrations/20260126113446_change_enum_list_name/migration.sql
+++ prisma/migrations/20260126113446_change_enum_list_name/migration.sql
@@ -1,16 +1,33 @@
-/*
-  Warnings:
-
-  - The values [paid] on the enum `rfStatus` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterEnum
 BEGIN;
+
+-- If a previous failed run left rfStatus_new around, remove it
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'rfstatus_new') THEN
+    DROP TYPE "rfStatus_new";
+  END IF;
+END $$;
+
+-- create the new enum
 CREATE TYPE "rfStatus_new" AS ENUM ('panding', 'refunded', 'failed');
-ALTER TABLE "public"."RefundBooking" ALTER COLUMN "refundStatus" DROP DEFAULT;
-ALTER TABLE "RefundBooking" ALTER COLUMN "refundStatus" TYPE "rfStatus_new" USING ("refundStatus"::text::"rfStatus_new");
+
+-- drop default, change column type
+ALTER TABLE "public"."RefundBooking"
+  ALTER COLUMN "refundStatus" DROP DEFAULT;
+
+ALTER TABLE "public"."RefundBooking"
+  ALTER COLUMN "refundStatus" TYPE "rfStatus_new"
+  USING ("refundStatus"::text::"rfStatus_new");
+
+-- rename old + new
 ALTER TYPE "rfStatus" RENAME TO "rfStatus_old";
 ALTER TYPE "rfStatus_new" RENAME TO "rfStatus";
-DROP TYPE "public"."rfStatus_old";
-ALTER TABLE "RefundBooking" ALTER COLUMN "refundStatus" SET DEFAULT 'panding';
+
+-- delete old enum
+DROP TYPE "rfStatus_old";
+
+-- restore default
+ALTER TABLE "public"."RefundBooking"
+  ALTER COLUMN "refundStatus" SET DEFAULT 'panding';
+
 COMMIT;
