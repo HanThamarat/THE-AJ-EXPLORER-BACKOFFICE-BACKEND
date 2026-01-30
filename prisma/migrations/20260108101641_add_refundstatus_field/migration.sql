97d2c20918cb10996b7e628412db2aceb0b03c45
HanThamarat <HanThamarat@gmail.com>
2026-01-30 14:32:40 +0700

    fix something

--- prisma/migrations/20260108101641_add_refundstatus_field/migration.sql
+++ prisma/migrations/20260108101641_add_refundstatus_field/migration.sql
@@ -1,5 +1,9 @@
--- CreateEnum
-CREATE TYPE "rfStatus" AS ENUM ('panding', 'paid', 'failed');
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'rfstatus') THEN
+    CREATE TYPE "rfStatus" AS ENUM ('panding', 'paid', 'failed');
+  END IF;
+END $$;
 
--- AlterTable
-ALTER TABLE "RefundBooking" ADD COLUMN     "refundStatus" "rfStatus" NOT NULL DEFAULT 'panding';
+ALTER TABLE "public"."RefundBooking"
+ADD COLUMN IF NOT EXISTS "refundStatus" "rfStatus" NOT NULL DEFAULT 'panding';
